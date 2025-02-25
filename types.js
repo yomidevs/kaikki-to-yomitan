25bd5c57f4b71db473d704f69afbd3e832504602
Stefan Vukovic <stefanvukovic44@gmail.com>
2025-02-25 14:06:56 +0100

    no ts errors

---
+++ types.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TermBankMeta = exports.TagBank = exports.TermBank = void 0;
+var TermBank = require("./node_modules/yomichan-dict-builder/src/types/yomitan/termbank");
+exports.TermBank = TermBank;
+var TagBank = require("./node_modules/yomichan-dict-builder/src/types/yomitan/tagbank");
+exports.TagBank = TagBank;
+var TermBankMeta = require("./node_modules/yomichan-dict-builder/src/types/yomitan/termbankmeta");
+exports.TermBankMeta = TermBankMeta;
