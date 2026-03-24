eaea36bd4ac7fa062319e1e11f9bca90ea30c16b
daxida <galmaw@hotmail.com>
2026-03-24 16:10:31 +0100

    [ja] Add archaic tag localization for testing

--- src/tags/tags_localization.rs
+++ src/tags/tags_localization.rs
@@ -17,13 +17,14 @@ pub fn localize_tag(lang: Lang, short_tag: &str) -> Option<(&'static str, &'stat
     }
 }
 
-/// Coverage: 8/258 tags (3.1%)
+/// Coverage: 9/258 tags (3.5%)
 fn localize_tag_ja(short_tag: &str) -> Option<(&'static str, &'static str)> {
     match short_tag {
         "v" => Some(("動", "動詞")),
         "n" => Some(("名", "名詞")),
         "adj" => Some(("形", "形容詞")),
         "dated" => Some(("旧", "古風")),
+        "arch" => Some(("古", "古語")),
         "name" => Some(("名前", "固有名詞")),
         "ptcl" => Some(("助", "助詞")),
         "sl" => Some(("俗", "俗語")),
